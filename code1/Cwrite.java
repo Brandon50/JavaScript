8d7791710b44697003fe707fb3a98665aa300828
Brandon50 <Brandonweston77@yahoo.com>
2016-10-25 09:47:16 -0700

    Add files via upload

---
+++ code1/Cwrite.java
@@ -0,0 +1,17 @@
+//
+
+class Cwrite {
+    public static void main (String a[]){
+        int i,j;
+        char c;
+        for(j = 0; j < 20000; j++){
+           for(i = 0; i < 80; i ++){
+               c = (char)(n % 48  +48);
+               
+               System.out.println(c);
+               n++;
+           }
+           System.out.println();
+       }
+    }
+}
